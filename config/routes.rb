860933b87c7513b3014f4c0ad2485be2668b1c35
Brian <ike76@me.com>
2014-08-21 21:35:55 -0700

    gems for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  root to: 'visitors#new'
+  
   resources :contacts, only: [:new, :create]
   resources :visitors, only: [:new, :create]
+  root to: 'visitors#new'
 end
